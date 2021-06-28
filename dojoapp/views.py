c91eb7d2d1ac79fcec0c8c19633e89552eae5ff0
Jordan Samsel <jordansamsel@Jordans-MacBook-Air.local>
2021-06-27 20:13:09 -0400

    dojo survey revisited

--- dojoapp/views.py
+++ dojoapp/views.py
@@ -1,14 +1,22 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def index(request):
     return render(request, 'index.html')
 
+def create(request):
+    request.session['dog_name'] = request.POST['dog_name']
+    request.session['dog_breed'] = request.POST['dog_breed']
+    request.session['dog_tricks'] = request.POST['dog_tricks']
+    request.session['comment'] = request.POST['comment']
+    
+    return redirect('/result')
+
 def result(request):
     context= {
-        'dog_name': request.POST['dog_name'],
-        'breed' : request.POST['dog_breed'],
-        'trick' : request.POST['dog_tricks'],
-        'comment' : request.POST['comment']
+        'dog_name': request.session['dog_name'],
+        'breed' : request.session['dog_breed'],
+        'trick' : request.session['dog_tricks'],
+        'comment' : request.session['comment']
     }
     return render(request, 'dog.html', context)
